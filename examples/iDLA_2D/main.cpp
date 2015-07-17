55109c4961e818718f2ee22c641dacd73347f3c0
vindar <vindar@users.noreply.github.com>
2015-07-17 13:11:08 +0200

    new demo examples

---
+++ examples/iDLA_2D/main.cpp
@@ -0,0 +1,31 @@
+/***********************************************
+ * Project : iDLA_2D
+ * date : Fri Jul 17 11:07:29 2015
+ * created via Vindar's libwizard.
+ ***********************************************/
+#include "stdafx.h" // pre-compiled header. 
+
+// *** Library mtools ***
+#include "mtools.hpp"
+using namespace mtools;
+
+// *** Library fltk ***
+#include "FL/Fl.H"      // add fltk headers this way
+#include "GL/glut.h"    // fltk glut
+#include "zlib.h"       // fltk zlib
+#include "png.h"        // fltk libpng
+#include "jpeglib.h"    // fltk libjpeg
+
+
+
+/* Change the 'subsystem' option to 'Windows' in Option/Linker/System 
+ * to remove the console. in this case, use Winmain() instead of main() */
+//int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
+int main(int argc, char *argv[]) 
+    {
+	cout << "Hello World\n"; 
+    return 0;
+	}
+	
+/* end of file main.cpp */
+
---
+++ examples/iDLA_2D/stdafx.h
@@ -0,0 +1,97 @@
+/***********************************************
+ * Project : iDLA_2D
+ * date : Fri Jul 17 11:07:29 2015
+ ***********************************************/
+
+#pragma message("Compiling pre-compiled headers for stdafx.h\n")
+#pragma once
+
+// STL
+#include <cstdlib>
+#include <cstdio>
+#include <ctime>
+#include <cmath>  
+#include <cwchar>
+#include <locale>
+
+#include <utility>
+#include <type_traits>
+#include <typeinfo>
+#include <string>
+#include <algorithm>
+#include <limits>
+#include <initializer_list>
+#include <chrono>
+
+#include <ostream>
+#include <fstream>
+#include <iostream>
+
+#include <mutex>
+#include <atomic>
+#include <thread>
+#include <condition_variable>
+
+#include <array>
+#include <vector>
+#include <deque>
+#include <forward_list>
+#include <list>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+
+
+// *** Library mtools ***
+#if defined (_MSC_VER) 
+#pragma warning( push )
+#pragma warning( disable : 4312 )
+#pragma warning( disable : 4319 )
+#endif
+#include "graphics/customcimg.hpp"
+#if defined (_MSC_VER) 
+#pragma warning( pop )
+#endif
+#include "mtools.hpp"
+
+// *** Library fltk ***
+#if defined (_MSC_VER) 
+#pragma warning( push )
+#pragma warning( disable : 4312 )
+#pragma warning( disable : 4319 )
+#endif
+#include "FL/Fl.H"
+#include "FL/Fl_Window.H"
+#include "FL/Fl_Double_Window.H"
+#include "FL/Fl_Box.H"
+#include "FL/Fl_Input.H"
+#include "FL/Fl_Button.H"
+#include "FL/Fl_Check_Button.H"
+#include "FL/Fl_Round_Button.H"
+#include "FL/Fl_Toggle_Button.H"
+#include "FL/Fl_Value_Slider.H"
+#include "FL/Fl_Scroll.H"
+#include "FL/Fl_Progress.H"
+#include "FL/Fl_Pack.H"
+#include "FL/Fl_Text_Display.H"
+#include "FL/fl_ask.H"
+#include "FL/Fl_Color_Chooser.H"
+#include "FL/Fl_File_Chooser.H"
+#include "FL/filename.H"
+#include "FL/fl_draw.H"
+#if defined (_MSC_VER) 
+#pragma warning( pop )
+#endif
+
+// *** Library fltk ***
+#include "zlib.h"       // fltk zlib
+#include "png.h"        // fltk libpng
+#include "jpeglib.h"    // fltk libjpeg
+
+
+
+/* end of file stdafx.h */
+
