28dd8046c848e889dad86216fc30527e6a6c9b40
Arvind Singh <vindar@users.noreply.github.com>
2015-07-21 00:04:05 +0200

    blop

--- mtools/headers/mtools/graphics/cimg_plugin.hpp
+++ mtools/headers/mtools/graphics/cimg_plugin.hpp
@@ -1,5 +1,5 @@
 
-/* Hack, needed so that the cimg plugin call be called from CImg.h 
+/* Hack, needed so that the cimg plugin can be called from CImg.h 
  * with the command #include "mtools/graphics/cimg_plugin.hpp". 
  */
  
