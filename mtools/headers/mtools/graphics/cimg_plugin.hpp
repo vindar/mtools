c8d11420106e3c19812093a3941b095e62a7099f
vindar <vindar@users.noreply.github.com>
2015-07-20 23:32:37 +0200

    Dummy cimg_plugin.hpp.

---
+++ mtools/headers/mtools/graphics/cimg_plugin.hpp
@@ -0,0 +1,8 @@
+
+/* Hack, needed so that the cimg plugin call be called from CImg.h 
+ * with the command #include "mtools/graphics/cimg_plugin.hpp". 
+ */
+ 
+#include "../../graphics/cimg_plugin.hpp"
+
+/* end of file */
