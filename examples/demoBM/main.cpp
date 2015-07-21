176a02834eb15799e76c8ab1469332e7a4d8fa44
vindar <vindar@users.noreply.github.com>
2015-07-21 13:21:49 +0200

    updated demo so that is uses suspend() correctly

--- examples/demoBM/main.cpp
+++ examples/demoBM/main.cpp
@@ -28,19 +28,18 @@ int main(int argc, char *argv[])
     P[PV][PF1][PF2];                    // insert everything in the plotter
     PV.interpolationLinear();           // use linear interpolation
     PV.hypograph(true);                 // fill the hypograph
-    PV.hypographOpacity(0.1f);          // but make it almost transparent
-    P.autoredraw(300);                  // the plotter window should redraw itself 5 times per second
+    PV.hypographOpacity(0.3f);          // but make it half transparent
+    P.autoredraw(60);                   // the plotter window should redraw itself at least every second (more in fact since it redraws after unsuspending)
     P.range().fixedAspectRatio(false);  // disable the fixed aspect ratio
     if (!autorange) P.range().setRange(fRect(-1.0e7, 5.0e8, -60000, 60000)); // set the range (if not automatic adjustment)
     P.startPlot();                      // display the plotter
     while (P.shown())                   // loop until the plotter window is closed
         {
         while (tab.size() < tab.capacity()) { pos += ((gen.rand_double0() < 0.5) ? -1 : 1); tab.push_back(pos); } // fill the vector with new steps of the walk until its capacity()
-        PV.suspend();      // disable the graph since we must reallocate memory (using suspend() instead of enable(false) remove flickering by not erasing the drawing).
-        //cout << tab.size() << "steps.\n";
-        tab.reserve(tab.capacity() + 1000000); // reserve additonnal space
-        PV.enable(true);   // enable again.
-        if (autorange) P.autorangeXY(); // autorange (this cause a redraw)
+        PV.suspend(true);      // suspend access to PV while we reallocate the vector memory.
+        tab.reserve(tab.capacity() + 1000000); // reserve more space
+        PV.suspend(false);     // resume access to the graph.
+        if (autorange) P.autorangeXY(); // autorange (causes a redraw)
         }
     return 0;
   	}
