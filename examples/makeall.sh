4d76e67c907d16f99734a610f37576ceb4189127
Arvind Singh <vindar@users.noreply.github.com>
2015-12-15 18:37:32 +0100

    improved script

--- examples/makeall.sh
+++ examples/makeall.sh
@@ -2,13 +2,15 @@
 #
 # Run the make command on all the sub-directories
 
+DIR=$PWD
+
 for d in */ ; do
 	echo
 	echo "-------------------------------------"
 	echo "Project $d"
 	cd $d
 	make $1
-	cd ..
+	cd $DIR
 done
 
 
