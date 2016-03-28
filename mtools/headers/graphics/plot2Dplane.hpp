377b64b6a6dc9e5aaa4879bade791a86fc891b4e
Arvind Singh <vindar@users.noreply.github.com>
2016-03-28 17:06:21 +0200

    first (new) version

--- mtools/headers/graphics/plot2Dplane.hpp
+++ mtools/headers/graphics/plot2Dplane.hpp
@@ -20,9 +20,10 @@
 
 #pragma once
 
+#include <iostream>
 
 #include "plotter2Dobj.hpp"
-#include "drawable2Dobject.hpp"
+#include "drawable2DInterface.hpp"
 #include "planedrawer.hpp"
 #include "rangemanager.hpp"
 #include "../misc/indirectcall.hpp"
@@ -45,9 +46,9 @@ namespace mtools
     *
     * @return  A Plot2DPlane<T>
     **/
-    template<typename T> Plot2DPlane<T> makePlot2DPlane(T & obj, std::string name = "Plane")
+    template<typename T> Plot2DPlane<T> makePlot2DPlane(T & obj, int nbthreads = 1, std::string name = "Plane")
         {
-        return Plot2DPlane<T>(obj, name);
+        return Plot2DPlane<T>(obj, nbthreads, name);
         }
 
 
@@ -61,10 +62,60 @@ namespace mtools
     *
     * @return  A Plot2DPlane<T>
     **/
-    template<typename T> Plot2DPlane<T> makePlot2DPlane(T * obj, std::string name = "Plane")
+    template<typename T> Plot2DPlane<T> makePlot2DPlane(T * obj, int nbthreads = 1, std::string name = "Plane")
         {
-        return Plot2DPlane<T>(obj, name);
+        return Plot2DPlane<T>(obj, nbthreads, name);
         }
+
+
+
+
+
+
+
+    /*
+
+    Drawable2DInterface() {}
+    virtual ~Drawable2DInterface() {}
+    virtual void setParam(mtools::fBox2 range, mtools::iVec2 imageSize) = 0;
+    virtual void resetDrawing() { if (nbThreads()>0) { MTOOLS_ERROR("resetDrawing should be overriden."); } return; }
+    virtual int drawOnto(Img<unsigned char> & im, float opacity = 1.0) = 0;
+    virtual int quality() const { if (nbThreads()>0) { MTOOLS_ERROR("quality() should be overriden."); } return 100; }
+    virtual void enableThreads(bool status) { return; }
+    virtual bool enableThreads() const { return false; }
+
+
+
+
+    virtual Drawable2DInterface * inserted(Fl_Group * & optionWin, int reqWidth);
+
+
+    /**
+    * This method is called when the owner of the widget removes it. This is the place where the
+    * option window should be deleted. The default implementation of this method simply calls
+    * "delete_widget(optionWin)" or does nothing if no option window was created and should suffice
+    * when using only simple fltk widgets (i.e. no timer or other thing that must be removed before
+    * the object goes out of scope).
+    * This method is called by the owner from the fltk thread.
+    *
+    * CAN BE OVERRIDEN IN DERIVED CLASS.
+    *
+    * @param [in,out]  optionWin   A pointer to the same option window that was given when inserted
+    *                              was called.
+    **/
+    //virtual void removed(Fl_Group * optionWin);
+
+    
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -75,7 +126,7 @@ namespace mtools
     * @tparam  T   Object which fulfills the same requirements as those needed by the PlaneDrawer
     *              class.
     **/
-    template< typename T > class  Plot2DPlane : public internals_graphics::Plotter2DObj
+    template< typename T > class Plot2DPlane : public internals_graphics::Plotter2DObj, protected internals_graphics::Drawable2DInterface
         {
 
         public:
@@ -87,9 +138,10 @@ namespace mtools
              *                      object must survive the plot.
              * @param   name        The name of the plot.
              **/
-            Plot2DPlane(T * obj, std::string name = "Plane") : internals_graphics::Plotter2DObj(name), _LD(nullptr), _encD(nullptr)
+            Plot2DPlane(T * obj, int nbthread = 1, std::string name = "Plane") : internals_graphics::Plotter2DObj(name), _LD(nullptr), _proImg(nullptr)
                 {
-                _LD = new PlaneDrawer<T>(obj);
+                _LD = new PlaneDrawer<T>(obj, nbthread);
+                _proImg = new ProgressImg();
                 }
 
 
@@ -100,19 +152,20 @@ namespace mtools
             *                      object must survive the plot.
             * @param   name        The name of the plot.
             **/
-            Plot2DPlane(T & obj, std::string name = "Plane") : internals_graphics::Plotter2DObj(name), _LD(nullptr), _encD(nullptr)
+            Plot2DPlane(T & obj, int nbthread = 1, std::string name = "Plane") : internals_graphics::Plotter2DObj(name), _LD(nullptr), _proImg(nullptr)
                 {
-                _LD = new PlaneDrawer<T>(&obj);
+                _LD = new PlaneDrawer<T>(&obj, nbthread);
+                _proImg = new ProgressImg();
                 }
 
 
             /**
             * Move constructor.
             **/
-            Plot2DPlane(Plot2DPlane && o) : internals_graphics::Plotter2DObj(std::move(o)), _LD((PlaneDrawer<T>*)o._LD), _LD(nullptr), _encD(nullptr)
+            Plot2DPlane(Plot2DPlane && o) : internals_graphics::Plotter2DObj(std::move(o)), _LD((PlaneDrawer<T>*)o._LD), _proImg(o._proImg)
                 {
-                o._LD = nullptr; // so that the plane drawer is not destroyed when the first object goes out of scope.
-                o._encD = nullptr;
+                o._LD = nullptr;     // so that the plane drawer is not destroyed when the first object goes out of scope.
+                o._proImg = nullptr;
                 }
 
 
@@ -121,116 +174,57 @@ namespace mtools
             **/
             virtual ~Plot2DPlane()
                 {
-                detach(); // detach from its owner if there is still one.
-                delete _LD; // remove the plane drawer
+                detach();   // detach from its owner if there is still one.
+                delete _LD;     // remove the plane drawer
+                delete _proImg; // and the progress image
                 }
 
 
-            /**
-            * Query the definition domain.
-            *
-            * @return  The current definition domain.
-            **/
-            fBox2 domain() const
-                {
-                return _LD->domain();
-                }
-
-            /**
-            * Query if the domain is the whole plane.
-            *
-            * @return  true if the domain is full, false if not.
-            **/
-            bool isDomainFull() const
-                {
-                return _LD->isDomainFull();
-                }
-
-
-            /**
-            * Queries if the domain is empty.
-            *
-            * @return  true if the domain is empty, false if not.
-            **/
-            bool isDomainEmpty() const
-                {
-                return _LD->isDomainEmpty();
-                }
-
-
-            /**
-            * Set the definition domain.
-            *
-            * @param   R   The new definition domain
-            **/
-            void domain(mtools::fBox2 R)
-                {
-                if (R == domain()) return;
-                _LD->domain(R);
-                if (isInserted())
-                    {
-                    enable(false); enable(true); resetDrawing(); // force update the range button on the object menu and then reset the drawing 
-                    }
-                }
-
-
-            /**
-            * Set a full definition Domain.
-            **/
-            void domainFull()
-                {
-                if (isDomainFull()) return;
-                _LD->domainFull();
-                if (isInserted())
-                    {
-                    enable(false); enable(true); resetDrawing(); // force update the range button on the object menu and then reset the drawing 
-                    }
-                }
-
-
-            /**
-            * Set an empty definition Domain.
-            **/
-            void domainEmpty()
-                {
-                if (isDomainEmpty()) return;
-                _LD->domainEmpty();
-                if (isInserted())
-                    {
-                    enable(false); enable(true); resetDrawing(); // force update the range button on the object menu and then reset the drawing 
-                    }
-                }
-
-
-
-            virtual fBox2 favouriteRangeX(fBox2 R) override
-                {
-                if ((_LD->isDomainEmpty() || _LD->isDomainFull())) return fBox2(); // no favourite range
-                return _LD->domain();
-                }
-
-
-            virtual fBox2 favouriteRangeY(fBox2 R) override
-                {
-                if ((_LD->isDomainEmpty() || _LD->isDomainFull())) return fBox2(); // no favourite range
-                return _LD->domain();
-                }
-
-
-            virtual bool hasFavouriteRangeX() override
-                {
-                return(!(_LD->isDomainEmpty() || _LD->isDomainFull())); // there is a favourite range if the domain is neither empy nor full
-                }
-
-
-            virtual bool hasFavouriteRangeY() override
-                {
-                return(!(_LD->isDomainEmpty() || _LD->isDomainFull())); // there is a favourite range if the domain is neither empy nor full
-                }
-
+  
 
 
         protected:
+
+            virtual void setParam(mtools::fBox2 range, mtools::iVec2 imageSize) override
+                {
+                if ((_proImg->height() != imageSize.X()) || (_proImg->width() != imageSize.Y()))
+                    {
+                    auto npimg  = new ProgressImg(imageSize.X(), imageSize.Y());
+                    _LD->setParameters(range, npimg);
+                    _LD->sync();
+                    delete _proImg;
+                    _proImg = npimg;
+                    return;
+                    }
+                _LD->setParameters(range, _proImg);
+                _LD->sync();
+                _LD->enable(_LD->enable());
+                }
+
+
+            virtual void resetDrawing() override
+                {
+                _LD->redraw();
+                _LD->sync();
+                }
+
+
+            virtual int drawOnto(Img<unsigned char> & im, float opacity = 1.0) override
+                {
+                int q = _LD->progress();
+                _proImg->blit(im,opacity,true);
+                return q;
+                }
+
+            virtual int quality() const override { return _LD->progress(); }
+
+
+            virtual void enableThreads(bool status) override { _LD->enable(status); _LD->sync();  }
+
+            virtual bool enableThreads() const override { return _LD->enable(); }
+
+            virtual int nbThreads() const { return _LD->nbThreads(); }
+
 
 
             /**
@@ -238,8 +232,7 @@ namespace mtools
             **/
             virtual void removed(Fl_Group * optionWin) override
                 {
-                delete _encD;
-                _encD = nullptr;
+                _LD->enable(false);
                 }
 
 
@@ -249,15 +242,14 @@ namespace mtools
             virtual internals_graphics::Drawable2DInterface * inserted(Fl_Group * & optionWin, int reqWidth) override
                 {
                 optionWin = nullptr;
-                _encD = new internals_graphics::EncapsulateDrawable2DObject(_LD, false);
-                return _encD;
+                return this;
                 }
 
 
         private:
 
-            PlaneDrawer<T> * _LD;                 // the plane drawer object
-            internals_graphics::EncapsulateDrawable2DObject * _encD;
+            PlaneDrawer<T> * _LD;
+            ProgressImg * _proImg;
 
         };
 
