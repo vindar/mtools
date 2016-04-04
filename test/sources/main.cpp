f263034112a8f1e8f614e808385a6497df48fe09
Arvind Singh <vindar@users.noreply.github.com>
2016-04-05 00:29:34 +0200

    testing

--- test/sources/main.cpp
+++ test/sources/main.cpp
@@ -92,10 +92,11 @@ int main(int argc, char *argv[])
     eight_neighbour = arg('e', false).info("use 8 neighbours adjacency");
     Grid({ 0,0 }) = N; N++; // initial particle 
     Plotter2D P;
-    //auto L = makePlot2DLattice(colorFct, "external DLA 2D"); 
-    auto L = makePlot2DPixel(colorFct, 2,"external DLA 2D");
+    auto L = makePlot2DLattice(colorFct, "external DLA 2D"); 
+    //auto L = makePlot2DPixel(colorFct, 6,"external DLA 2D");
     P[L];
     P.autoredraw(autoredraw);
+    P.sensibility(10);
     P.startPlot();
     watch("# of particles", N);
     watch("cluster radius", maxrad);
