56b5af2e256c75147cb96b020b6ca6c194547cd3
Arvind Singh <vindar@users.noreply.github.com>
2017-01-24 17:03:20 +0100

    mobius class, first version

--- mtools/headers/maths/complex.hpp
+++ mtools/headers/maths/complex.hpp
@@ -34,6 +34,98 @@ namespace mtools
 
 
 
+
+	template<typename  T> class Mobius
+		{
+
+		public:
+
+		/** Default constructor. set the identity transformation. */
+		Mobius() : a((T)1.0), b(T(0.0)), c((T)0.0), d(T(1.0)) {}
+
+		/**
+		 * Constructor. constuct the mobius transformation z -> (az + b)/(cz + d).
+		 */
+		Mobius(const mtools::complex<T> & aa,const mtools::complex<T> & bb,const mtools::complex<T> & cc, const mtools::complex<T> & dd) : a(aa), b(bb), c(cc), d(dd) {}
+		
+
+		/**   
+		/* Mobius tranformation z -> (z - c)/(conj(c)z - 1).
+		 * This swaps c and 0  while preserving the unit disk if |c| < 1.
+		 **/
+		Mobius(const mtools::complex<T> & c) : a((T)1.0), b(-c), c(std::conj(c)), d((T)(-1.0)) {}
+
+
+		/**
+		 * Composition of two mobius tranformation.
+		 */
+		Mobius operator*(const Mobius & M) const 
+			{
+			return Mobius(a*M.a + b*M.c, a*M.b + b*M.d, c*M.a + d*M.c, c*M.b + d*M.d);
+			}
+
+
+		/**
+		 * Compute the image of a point.
+		 */
+		mtools::complex<T> operator*(const mtools::complex<T> & z) const
+			{
+			return (a * z + b)/(c * z + d);
+			}
+
+
+		/**
+		 * Return the invert transformation.
+		 */
+		Mobius invert() const
+			{
+			return Mobius(d, -b, -c, a);
+			}
+
+
+		/**
+		 * Compute the image of a circle by the mobius transformation. It is again a circle (but beware
+		 * that the new center is not the image of the originial center).
+		 */
+		std::pair<std::complex<T>, T> imageCircle(const std::complex<T> & center, const T & rad) const
+			{
+			return std::pair<std::complex<T>, T>(
+				((a*center + b)*(std::conj(c*center + d)) - rad*rad*a*(std::conj(c))) / (std::norm(c*center + d) - rad*rad*(std::norm(c))),
+				(rad*(std::abs(a*d - b*c))) / (std::abs(std::norm(c*center + d) - rad*rad*norm(c)))
+				);
+			}
+
+
+		/**
+		* serialise/deserialize the tranformation. Works with boost and with the custom serialization classes
+		* OArchive and IArchive. the method performs both serialization and deserialization.
+		**/
+		template<typename U> void serialize(U & Archive, const int version = 0)
+			{
+			Archive & a & b & c & d;
+			}
+
+
+		/**
+		* Print the the vector into a string.
+		**/
+		std::string toString() const
+			{
+			std::string s("Mobius[");
+			return(s + mtools::toString(a) + "," + mtools::toString(b) + "," + mtools::toString(c) + "," + mtools::toString(d) + "]");
+			}
+
+
+
+		mtools::complex<T> a;
+		mtools::complex<T> b;
+		mtools::complex<T> c;
+		mtools::complex<T> d;
+
+		};
+
+
+
 	}
 
 /* end of file */
