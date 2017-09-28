83d1fcce4115fc562c391cb8528922924475dd9f
Arvind Singh <vindar@users.noreply.github.com>
2017-09-28 14:52:13 +0200

    removed fltk and mtools form the examples precompiled headers

--- examples/SRW_1D/stdafx.h
+++ examples/SRW_1D/stdafx.h
@@ -41,59 +41,6 @@
 #include <unordered_set>
 #include <unordered_map>
 
-// *** Library mtools ***
-// Uncomment the following line to disable mtools console and use stdout instead
-// #define MTOOLS_BASIC_CONSOLE
-#include "mtools.hpp"
-
-// *** Library fltk ***
-#if defined (_MSC_VER) 
-#pragma warning( push )
-#pragma warning( disable : 4312 )
-#pragma warning( disable : 4319 )
-#endif
-#include "FL/Fl.H"
-#include "FL/Fl_Window.H"
-#include "FL/Fl_Double_Window.H"
-#include "FL/Fl_Box.H"
-#include "FL/Fl_Input.H"
-#include "FL/Fl_Button.H"
-#include "FL/Fl_Check_Button.H"
-#include "FL/Fl_Round_Button.H"
-#include "FL/Fl_Toggle_Button.H"
-#include "FL/Fl_Value_Slider.H"
-#include "FL/Fl_Scroll.H"
-#include "FL/Fl_Progress.H"
-#include "FL/Fl_Pack.H"
-#include "FL/Fl_Text_Display.H"
-#include "FL/fl_ask.H"
-#include "FL/Fl_Color_Chooser.H"
-#include "FL/Fl_File_Chooser.H"
-#include "FL/filename.H"
-#include "FL/fl_draw.H"
-#if defined (_MSC_VER) 
-#pragma warning( pop )
-#endif
-
-// *** Library cairo ***
-#include "cairo.h"
-
-// *** Library freetype ***
-//#include "freetype.h"
-
-// *** Library libjpeg ***
-#include "jpeglib.h"
-
-// *** Library libpng ***
-#include "png.h"
-
-// *** Library pixman ***
-#include "pixman.h"
-
-// *** Library zlib ***
-#include "zlib.h"
-
-
 
 /* end of file stdafx.h */
 
