740ac02e115905ce4f7049bd4e3d164fa16bd6b7
Arvind Singh <vindar@users.noreply.github.com>
2018-01-10 16:35:51 +0100

    good

--- clean_build.py
+++ clean_build.py
@@ -1,7 +1,10 @@
 import shutil
 import os
-
+		
 if (os.path.isdir("build")):
 	shutil.rmtree('build')
 	
 os.makedirs('build')
+with open('build/build_directory', 'a'):
+        os.utime('build/build_directory', None)
+
