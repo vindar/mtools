ae197dbcbd5cde2f6468234e456d944bd5225ec4
Arvind Singh <vindar@users.noreply.github.com>
2018-01-16 12:16:06 +0100

    good

--- cmake/FindFLTK_alt.cmake
+++ cmake/FindFLTK_alt.cmake
@@ -5,77 +5,70 @@
 #
 # if found, set FLTK_FOUND and FLTK_alt_FOUND
 #
-# define also
-#
-#     FLTK_OPTIONS
-#     FLTK_LIBRARIES
-#     FLTK_INCLUDE_DIR
+#     FLTK_OPTIONS      - list of option to pass to the compiler
+#     FLTK_LIBRARIES    - list of libraries
+#     FLTK_INCLUDE_DIR  - fltk include directory (contains the /FL sub-directory)
 #
 
 
-macro(findfltklib FLTKLIBNAME FLTKLIBFILE)
-	find_library(${FLTKLIBNAME}  ${FLTKLIBFILE})
-	if(NOT ${FLTKLIBNAME})
-		message(FATAL_ERROR "${FLTKLIBFILE}.lib not found (using vcpkg)")
+
+
+macro(find_library_flag LIBNAME LIBFILE FLAG)
+	set(TMPLIBNAME "TMPLIBNAME-NOTFOUND")
+	find_library(TMPLIBNAME ${LIBFILE})
+	if(NOT TMPLIBNAME)
+		set(FLAG 0)
+	else()
+		set(${LIBNAME} ${${LIBNAME}} ${TMPLIBNAME})
 	endif()	
 endmacro()
 
 
-if (_vcpkg_dir)
+if (FLTK_alt_FOUND) 
+	return()
+endif()
 
-	# using vcpkg, bypass usual find method to allwo both debug and release build
-	
-	findfltklib(FLTK_MAIN_LIBRARY_RELEASE  fltk)
-	findfltklib(FLTK_MAIN_LIBRARY_DEBUG  fltkd)
-	
-	findfltklib(FLTK_GL_LIBRARY_RELEASE  fltk_gl)
-	findfltklib(FLTK_GL_LIBRARY_DEBUG  fltk_gld)
-	
-	findfltklib(FLTK_IMAGES_LIBRARY_RELEASE  fltk_images)
-	findfltklib(FLTK_IMAGES_LIBRARY_DEBUG  fltk_imagesd)
-	
-	findfltklib(FLTK_FORMS_LIBRARY_RELEASE  fltk_forms)
-	findfltklib(FLTK_FORMS_LIBRARY_DEBUG  fltk_formsd)
 
+if (VCPKG_TOOLCHAIN)
+	# use vcpkg to find fltk
+	set (FOUND_ALL_LIB 1)
+	set (FLTKGL_LIB 1)
+	set (FLTK_LIBRARY_RELEASE "")
+	set (FLTK_LIBRARY_DEBUG "")
+	
+	find_library_flag(FLTK_LIBRARY_RELEASE  fltk FOUND_ALL_LIB)
+	find_library_flag(FLTK_LIBRARY_RELEASE  fltk_images FOUND_ALL_LIB)
+	find_library_flag(FLTK_LIBRARY_RELEASE  fltk_forms FOUND_ALL_LIB)
+	find_library_flag(FLTK_LIBRARY_RELEASE  fltk_gl FLTKGL_LIB)
+	find_library_flag(FLTK_LIBRARY_DEBUG  fltkd FOUND_ALL_LIB)
+	find_library_flag(FLTK_LIBRARY_DEBUG  fltk_imagesd FOUND_ALL_LIB)
+	find_library_flag(FLTK_LIBRARY_DEBUG  fltk_formsd FOUND_ALL_LIB)
+	find_library_flag(FLTK_LIBRARY_DEBUG  fltk_gld FLTKGL_LIB)
+		
 	find_path(FLTK_INCLUDE_DIR "FL/Fl.H")
-	if(NOT FLTK_INCLUDE_DIR)
-		message(FATAL_ERROR "FL/Fl.H not found (using vcpkg)")
-	endif()	
-		
-	set(FLTK_LIBRARY_RELEASE "${FLTK_MAIN_LIBRARY_RELEASE}" "${FLTK_GL_LIBRARY_RELEASE}" "${FLTK_IMAGES_LIBRARY_RELEASE}" "${FLTK_FORMS_LIBRARY_RELEASE}" "comctl32" "opengl32" "glu32")
-	set(FLTK_LIBRARY_DEBUG "${FLTK_MAIN_LIBRARY_DEBUG}" "${FLTK_GL_LIBRARY_DEBUG}" "${FLTK_IMAGES_LIBRARY_DEBUG}" "${FLTK_FORMS_LIBRARY_DEBUG}" "comctl32" "opengl32" "glu32")
-		
-	# Create FLTK_LIBRARIES from FLTK_LIBRARY_DEBUG and FLTK_LIBRARY_RELEASE
-	select_library_configurations(FLTK)		
-		
-	get_filename_component(FLTK_R_PATH "${FLTK_MAIN_LIBRARY_RELEASE}" DIRECTORY)
-	get_filename_component(FLTK_D_PATH "${FLTK_MAIN_LIBRARY_DEBUG}" DIRECTORY)		
-	message(STATUS "Found FLKT (via vcpkg): release [${FLTK_R_PATH}]  debug [${FLTK_D_PATH}]")
-		
-	set(FLTK_FOUND 1)
-	set(FLTK_alt_FOUND 1)
+	
+	if (FOUND_ALL_LIB AND FLTK_INCLUDE_DIR)		
+		set(FLTK_LIBRARY_RELEASE "${FLTK_LIBRARY_RELEASE}" "comctl32")
+		set(FLTK_LIBRARY_DEBUG "${FLTK_LIBRARY_DEBUG}" "comctl32")		
+		if (FLTKGL_LIB)
+			set(FLTK_LIBRARY_RELEASE "${FLTK_LIBRARY_RELEASE}" "opengl32" "glu32")
+			set(FLTK_LIBRARY_DEBUG "${FLTK_LIBRARY_DEBUG}" "opengl32" "glu32")
+		endif()	
+		select_library_configurations(FLTK) # create FLTK_LIBRARIES from FLTK_LIBRARY_DEBUG and FLTK_LIBRARY_RELEASE		
+		list (GET FLTK_LIBRARY_RELEASE 0 RELPATH)
+		list (GET FLTK_LIBRARY_DEBUG 0 DEBPATH)		
+		message(STATUS "Found FLTK (via vcpkg): release [${RELPATH}]  debug [${DEBPATH}]")		
+		set(FLTK_FOUND 1)
+	endif()
+endif()
 
-	mark_as_advanced(FLTK_D_PATH)
-	mark_as_advanced(FLTK_R_PATH)
-	mark_as_advanced(FLTK_MAIN_LIBRARY_RELEASE)
-	mark_as_advanced(FLTK_MAIN_LIBRARY_DEBUG)
-	mark_as_advanced(FLTK_GL_LIBRARY_RELEASE)
-	mark_as_advanced(FLTK_GL_LIBRARY_DEBUG)
-	mark_as_advanced(FLTK_IMAGES_LIBRARY_RELEASE)
-	mark_as_advanced(FLTK_IMAGES_LIBRARY_DEBUG)
-	mark_as_advanced(FLTK_FORMS_LIBRARY_RELEASE)
-	mark_as_advanced(FLTK_FORMS_LIBRARY_DEBUG)
-	
-	
-else()
+
+if (NOT FLTK_FOUND)
 
 	# try to find fltk-config	
 	find_program(FLTKCONFIG_PROG fltk-config)
 
 	if (FLTKCONFIG_PROG)
-
-		set(FLTK_FOUND 1)
-		set(FLTK_alt_FOUND 1)
 
 		execute_process(COMMAND "${FLTKCONFIG_PROG}" "--cxxflags" OUTPUT_VARIABLE FLTK_OPTIONS OUTPUT_STRIP_TRAILING_WHITESPACE)
 			string(REGEX REPLACE "\n$" "" FLTK_OPTIONS "${FLTK_OPTIONS}")
@@ -86,24 +79,33 @@ else()
 			string(STRIP ${FLTK_LIBRARIES} FLTK_LIBRARIES)
 
 		set(FLTK_INCLUDE_DIR "")
-		message(STATUS "Found FLKT (via fltk-config): ${FLTKCONFIG_PROG}")
+		message(STATUS "Found FLKT (via fltk-config): [${FLTKCONFIG_PROG}]")
+		set(FLTK_FOUND 1)
 
 	else ()
 
-		# try to find fltk with the cmake bundled FindFLTK module
+		# try to find fltk with the official FindFLTK module
 		message(STATUS "fltk-config NOT found, looking for FLTK...")
 		set(FLTK_SKIP_FLUID TRUE)
 		set(FLTK_SKIP_FORMS TRUE)
 		find_package(FLTK)
-		if (FLTK_FOUND)
-			set(FLTK_alt_FOUND 1)
-		endif()
 
 	endif()
 
-	mark_as_advanced(FLTKCONFIG_PROG)
-
 endif()
 
+
+if (FLTK_FOUND)
+	set(FLTK_alt_FOUND 1)
+else()
+	if (FLTK_alt_FIND_REQUIRED)
+		message(SEND_ERROR "FLTK NOT found")
+	else()
+		message(STATUS "FLTK NOT found.")
+	endif()
+endif()
+
+
+mark_as_advanced(TMPLIBNAME)
 
 
