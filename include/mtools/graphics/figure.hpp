a58d6d02ebcc3f5dd1a067833ca056e92513bf91
Arvind Singh <vindar@users.noreply.github.com>
2018-01-25 16:57:12 +0100

    added figure.hpp header file

---
+++ include/mtools/graphics/figure.hpp
@@ -0,0 +1,53 @@
+/** @file figure.hpp */
+//
+// Copyright 2015 Arvind Singh
+//
+// This file is part of the mtools library.
+//
+// mtools is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with mtools  If not, see <http://www.gnu.org/licenses/>.
+
+
+#pragma once
+
+#include "../misc/internal/mtools_export.hpp"
+#include "../misc/error.hpp"
+#include "../maths/vec.hpp"
+#include "../maths/box.hpp"
+#include "image.hpp"
+#include "font.hpp"
+
+
+namespace mtools
+{
+    /**
+     * Class for creating Figure = vector graphics images. 
+     *
+     **/
+	class Figure
+	{
+    public:
+
+        Figure() {}
+        ~Figure()
+
+	private:
+
+	};
+
+
+}
+
+
+/* end of file */
+
