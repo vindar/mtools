390031004c5930e8cd218ad3e27776125141a762
Arvind Singh <vindar@users.noreply.github.com>
2019-09-22 10:58:02 +0200

    blop

--- test/main.cpp
+++ test/main.cpp
@@ -14,10 +14,19 @@ int main(int argc, char *argv[])
 
 	Plotter2D plotter;
 
+	std::cout << "HELLO WORLD3\n";
+
 	auto P = makePlot2DFun(f, "sin");
 
+	std::cout << "HELLO WORLD4\n";
+
 	plotter[P];
+
+	std::cout << "HELLO WORLD5\n";
+
 	plotter.plot();
+
+	std::cout << "HELLO WORLD6\n";
 
 	MTOOLS_SWAP_THREADS(argc, argv);         // required on OSX, does nothing on Linux/Windows
 	return 0;
