0464497418542d581408eb73e59058970141da5f
Arvind Singh <vindar@users.noreply.github.com>
2022-08-02 00:53:47 +0200

    blop

--- test/main.cpp
+++ test/main.cpp
@@ -4,6 +4,9 @@ using namespace mtools;
 #include "buddha.h"
 
 
+
+mtools::MT2004_64 gen(123);
+
 using namespace std::placeholders;
 
 
@@ -27,9 +30,7 @@ ImageDisplay ID(LX, LY);
 
 float a = 0;
 
-
 char cb[1000000];
-
 
 
 
@@ -50,21 +51,63 @@ void testblend()
 
 	ImageDisplay ID(LX, LY);
 
-	Image src(200, 200);
-	tgx::Image<tgx::RGB32> tgx_src(src);
-	tgx_src.fillScreenHGradient(tgx::RGB32_Purple, tgx::RGB32_Orange);
-	tgx_src.fillCircle({ 100, 100 }, 80, tgx::RGB32_Salmon, tgx::RGB32_Black);
-
 	Image dst(320, 240);
 	tgx::Image<tgx::RGB32> tgx_dst(dst);
-	tgx_dst.fillScreenVGradient(tgx::RGB32_Green, tgx::RGB32_White);
 
-	//tgx_dst.blit(tgx_src, { 60 , 20 }, mult_op);
+	tgx_dst.fillScreen(tgx::RGB32_Black);
+
+	/*
+	tgx_dst.drawQuadBezier({10,10}, {100, 10}, {200,100}, 1.0f, true, tgx::RGB32_White, 0.5f);
+
+	tgx_dst.drawQuadBezier({ 10,10 }, { 100, 10 }, { 200,100 }, 100.0f, true, tgx::RGB32_Red);
 
 
-	tgx_dst.blitScaledRotated(tgx_src, tgx_src.dim()/2, tgx_dst.dim()/2, 1.0f, 45, [](tgx::RGB32 src, tgx::RGB32 dst) { return tgx::RGB32(src.G, src.R, src.B); });
+	tgx_dst.drawCubicBezier({ 10,10 }, { 100, 10 }, { 200,100 }, { 0,200 }, true, tgx::RGB32_Green);
 
-	//tgx_dst.copyFrom(tgx_dst, [](tgx::RGB32 src, tgx::RGB32 dst) {return tgx::RGB32(src.G, src.R, src.B); });
+
+	*/
+	tgx::iVec2 tabP[4] = { {20, 20}, {100, 20}, {100, 100}, {20, 100}  };
+
+
+	tgx::iVec2 tabP2[6] = { {20, 20}, {60, 20}, {100, 20}, {100, 100}, {60,100}, {20, 100} };
+
+	
+	//tgx_dst.drawQuadSpline(4, tabP, true, tgx::RGB32_Blue, 0.4f);
+	
+	tgx_dst.drawClosedSpline(4, tabP, tgx::RGB32_Orange, 0.4f);
+
+	tgx_dst.drawClosedSpline(6, tabP2, tgx::RGB32_Red, 0.4f);
+
+
+
+	tgx_dst.drawCubicSpline(4, tabP, true, tgx::RGB32_Yellow, 0.4f);
+
+
+	//tgx_dst.drawPolygon(4, tabP,  tgx::RGB32_Red, 0.5f);
+	
+	//tgx_dst.drawPolygon(4, tabP,  tgx::RGB32_Yellow, 0.5f);
+
+
+	int k = 5000; 
+	for (int u = 0; u < k; u++)
+		{
+		tgx_dst.drawPixel((int)(Unif(gen) * 100), (int)(Unif(gen) * 100), tgx::RGB32_Olive);
+		}
+
+
+	tgx_dst.drawPixel(70,70, tgx::RGB32_Black);
+
+	int st;
+	/*
+	mtools::Chrono ch;
+	for (int i = 0; i < 1; i++)
+		{
+		st = tgx_dst.fill<100000>({ 70,70 }, tgx::RGB32_Blue);
+		}
+	cout << ch << "\n";
+	*/
+	cout << "stack used = " << tgx_dst.fill<100000>({ 70,70 }, tgx::RGB32_Blue);
+	cout << "stack used = " << tgx_dst.fill<100000>({70,70}, tgx::RGB32_Blue);
 
 
 	ID.setImage(&dst);
@@ -74,136 +117,20 @@ void testblend()
 
 
 
-template<typename color_t> 
-void drawBottomRightCorner(tgx::Image<color_t> & im, int x, int y, int r, color_t color)
-	{
-	tgx::Image<color_t> sub_im = im.getCrop({ x, x + r + 1, y, y + r + 1}); // create a sub image for clipping
-	sub_im.drawCircle({ 0,0 }, r, color); // draw the circle on the sub image 
-	}
-
-class TT
-{
-public:
 
 
-	TT(double c) : a(c) {}
 
 
-	double ff(double x)
-		{
-		return a * x;
-		}
 
-
-	RGBc getcol(int64 x, int64 y)
-		{
-		return (x * x + y * y < 100*a) ? RGBc::c_Green : RGBc::c_Transparent;
-		}
-
-
-private:
-	double a; 
-};
-
-
-RGBc getcol(int64 x, int64 y)
-	{
-	return (x * x + y * y < 100) ? RGBc::c_Red : RGBc::c_Transparent;
-	}
-
-
-        template<typename T> class GetC
-            {
-            static void * dumptr;
-            typedef typename std::decay<mtools::RGBc>::type decayrgb;
-
-			/*
-            template<typename U> static decltype((*(U*)(0)).getColor(iVec2{ 0,0 }, dumptr)) vers1(int);
-            template<typename> static metaprog::no vers1(...);
-            static const bool version1 = std::is_same<typename std::decay<decltype(vers1<T>(0))>::type, decayrgb>::value;
-
-
-			
-            template<typename U> static decltype((*(U*)(0)).getColor(iVec2{ 0,0 })) vers2(int);
-            template<typename> static metaprog::no vers2(...);
-            static const bool version2 = std::is_same<typename std::decay<decltype(vers2<T>(0))>::type, decayrgb >::value;
-
-            template<typename U> static decltype((*(U*)(0)).getColor(0, 0, dumptr)) vers3(int);
-            template<typename> static metaprog::no vers3(...);
-            static const bool version3 = std::is_same<typename std::decay<decltype(vers3<T>(0))>::type, decayrgb >::value;
-
-            template<typename U> static decltype((*(U*)(0)).getColor(0, 0)) vers4(int);
-            template<typename> static metaprog::no vers4(...);
-            static const bool version4 = std::is_same<typename std::decay<decltype(vers4<T>(0))>::type, decayrgb >::value;
-
-			
-            template<typename U> static decltype((*(U*)(0))(iVec2{ 0,0 }, dumptr)) vers5(int);
-            template<typename> static metaprog::no vers5(...);
-            static const bool version5 = std::is_same<typename std::decay<decltype(vers5<T>(0))>::type, decayrgb >::value;
-
-			*/
-			
-            template<typename U> static decltype((*(U*)(0))(iVec2{ 0,0 })) vers6(int);
-            template<typename> static metaprog::no vers6(...);
-            static const bool version6 = std::is_same<typename std::decay<decltype(vers6<T>(0))>::type, decayrgb >::value;
-
-			/*
-            template<typename U> static decltype((*(U*)(0))(0, 0, dumptr)) vers7(int);
-            template<typename> static metaprog::no vers7(...);
-            static const bool version7 = std::is_same<typename std::decay<decltype(vers7<T>(0))>::type, decayrgb >::value;
-
-            template<typename U> static decltype((*(U*)(0))(0, 0)) vers8(int);
-            template<typename> static metaprog::no vers8(...);
-            static const bool version8 = std::is_same<typename std::decay<decltype(vers8<T>(0))>::type, decayrgb >::value;
-			*/
-
-
-            public:
-
-				static const bool has_getColor = version1; // | version2 | version3 | version4 | version5 | version6 | version7 | version8;
-
-            };
 
 int main(int argc, char *argv[])
 {
 	MTOOLS_SWAP_THREADS(argc, argv);         // required on OSX, does nothing on Linux/Windows
+	 
 
+	testblend(); 
+	return 0; 
 
-	//testblend(); 
-	//return 0; 
-
-
-	TT tt(0.5);
-
-	{
-
-
-		auto hh = std::bind(&TT::ff, &tt, _1);
-		cout << hh(3.0);
-
-		auto P1 = makePlot2DFun([&](double x) {return x * x; }, std::string("lambda"));
-		auto P2 = makePlot2DFun(&TT::ff, tt, 0, 1, "lambda");
-
-		//auto P3 = makePlot2DLattice(&TT::getcol, &tt, "zz");	
-		
-		auto bb = std::bind(&TT::getcol, tt, std::placeholders::_1, std::placeholders::_2);
-		
-
-		cout << "has color : " << GetC<decltype(bb)>::has_getColor << "\n";
-
-
-		//auto P3 = makePlot2DLattice(bb);
-			
-
-		//cout << bb(2, 4);
-
-		Plotter2D plotter;
-		plotter[P1];
-		plotter[P2];
-		//plotter[P3];
-		plotter.range().setRange({ -20,20, -20, 20 });
-		plotter.plot();
-	}
 
 
 
@@ -240,7 +167,6 @@ int main(int argc, char *argv[])
 		tgxim.drawLine({ 100, 100 },{200, 100},tgx::RGB32_Green);
 		tgxim.drawLine({ 100, 100 }, { 100, 200 }, tgx::RGB32_Green);
 
-		drawBottomRightCorner(tgxim, 100, 100, 50, tgx::RGB32_Black);
 
 
 		ID.redrawNow();
